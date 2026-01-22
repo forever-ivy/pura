11db57a743400f10a4b5be95e1b43adcb781909c
forever-ivy <yezzzus@163.com>
2026-01-23 01:17:25 +0800

    chore: update dependencies and add new packages
    - Added @radix-ui/react-checkbox, @radix-ui/react-popover, cmdk, and shadcn to dependencies.
    - Updated existing dependencies to their latest versions.

--- components/activities/activities-logger.tsx
+++ components/activities/activities-logger.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -10,14 +8,32 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 const activityTypes = [
   { id: "meditation", name: "冥想" },
@@ -28,37 +44,43 @@ const activityTypes = [
   { id: "therapy", name: "咨询" },
 ];
 
+const activityTypeIds = activityTypes.map((a) => a.id) as [
+  "meditation",
+  "exercise",
+  "walking",
+  "reading",
+  "journaling",
+  "therapy",
+];
+
 interface ActivityLoggerProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
 export function ActivityLogger({ open, onOpenChange }: ActivityLoggerProps) {
-  const [type, setType] = useState("");
-  const [name, setName] = useState("");
-  const [duration, setDuration] = useState("");
-  const [description, setDescription] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const schema = z.object({
+    type: z.enum(activityTypeIds),
+    description: z.preprocess(
+      (value) => (value === "" ? undefined : value),
+      z.string().min(1).optional(),
+    ),
+    name: z.string().min(1),
+  });
 
-  const handleSubmit = (e: React.FormEvent) => {
-    setTimeout(() => {
-      console.log({
-        type,
-        name,
-        duration,
-        description,
-      });
+  type FormData = z.infer<typeof schema>;
 
-      //Rest fields
-      setType("");
-      setName("");
-      setDuration("");
-      setDescription("");
-      setIsLoading(false);
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      type: activityTypeIds[0],
+      description: "",
+      name: "",
+    },
+  });
 
-      alert("活动已记录（模拟）！");
-      onOpenChange(false);
-    }, 1000);
+  const handleSubmit = (data: FormData) => {
+    console.log(data);
   };
 
   return (
@@ -68,50 +90,135 @@ export function ActivityLogger({ open, onOpenChange }: ActivityLoggerProps) {
           <DialogTitle>记录活动</DialogTitle>
           <DialogDescription>记录你的身心健康活动</DialogDescription>
         </DialogHeader>
-        <form action="" onSubmit={handleSubmit}>
-          <div className="space-y-2">
-            <Label>活动类型</Label>
-            <Select value={type} onValueChange={setType}>
-              <SelectTrigger>
-                <SelectValue placeholder="选择活动类型" />
-              </SelectTrigger>
-              <SelectContent>
-                {activityTypes.map((activity) => (
-                  <SelectItem key={activity.id} value={activity.id}>
-                    {activity.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-        </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-5"
+          >
+            {/* <div className="space-y-2">
+              <Label>活动类型</Label>
+              <Select value={type} onValueChange={setType}>
+                <SelectTrigger>
+                  <SelectValue placeholder="选择活动类型" />
+                </SelectTrigger>
+                <SelectContent>
+                  {activityTypes.map((activity) => (
+                    <SelectItem key={activity.id} value={activity.id}>
+                      {activity.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div> */}
 
-        <div className="space-y-2">
-          <Label>名称</Label>
-          <Input
-            value={name}
-            onValueChange={(e) => setName(e.target.value)}
-            placeholder="例如：早晨冥想、晚间散步…"
-          />
-        </div>
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>活动类型</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          className={cn(
+                            "w-[200px] justify-between",
+                            !field.value && "text-muted-foreground",
+                          )}
+                          role="combobox"
+                          variant="outline"
+                        >
+                          {field.value
+                            ? activityTypes.find(
+                                (activityType) =>
+                                  activityType.id === field.value,
+                              )?.name
+                            : "选择活动类型"}
+                          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0 ">
+                      <Command>
+                        <CommandInput
+                          className="h-8 p-2"
+                          placeholder="寻找活动..."
+                        />
+                        <CommandList>
+                          <CommandEmpty>No language found.</CommandEmpty>
+                          <CommandGroup>
+                            {activityTypes.map((activityType) => (
+                              <CommandItem
+                                key={activityType.id}
+                                onSelect={() => {
+                                  form.setValue("type", activityType.id, {
+                                    shouldDirty: true,
+                                    shouldValidate: true,
+                                  });
+                                }}
+                                value={activityType.name}
+                              >
+                                <Check
+                                  className={cn(
+                                    "mr-2 h-4 w-4",
+                                    activityType.id === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0",
+                                  )}
+                                />
+                                {activityType.name}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <div className="space-y-2">
-          <Label>描述（选填）</Label>
-          <Input
-            value={description}
-            onValueChange={(e) => setDescription(e.target.value)}
-            placeholder="感觉如何？"
-          />
-        </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>名称</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="例如：早晨冥想、晚间散步…"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <div className="flex justify-end gap-2 ">
-          <Button type="button" variant="ghost">
-            取消
-          </Button>
-          <Button type="submit" disabled>
-            保存活动
-          </Button>
-        </div>
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>描述（选填）</FormLabel>
+                  <FormControl>
+                    <Input type="text" placeholder="感觉如何？" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex justify-end gap-2 ">
+              <Button type="button" variant="ghost">
+                取消
+              </Button>
+              <Button type="submit">保存活动</Button>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
